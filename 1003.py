a9e384765d5bbad7cb1870c1d447c934a811d1e4
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-02-15 17:42:56 +0900

    수정: 1003: 전역변수 추가, sys.stdin.readline 함수 추가

--- 1003.py
+++ 1003.py
@@ -1,15 +1,19 @@
+import sys
+
 p0, p1 = 0, 0
 print("t값을 받습니다")
-t = input()
+# t = input()
+t = sys.stdin.readline()
 t = int(t)
 
 def fibo(n):
+    global p0, p1
     if(n == 0):
-        print("0")
+        # print("0")
         p0 += 1
         return 0
     elif(n == 1):
-        print("1")
+        # print("1")
         p1 += 1
         return 1
     else:
@@ -18,7 +22,8 @@ def fibo(n):
 for i in range(0, t):
     p0, p1 = 0, 0
     print("m값을 받습니다")
-    m = input()
+    # m = input()
+    m = sys.stdin.readline()
     m = int(m)
     fibo(m)
     print(p0, p1)
