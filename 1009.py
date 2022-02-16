3bea7baa11b90669b05d4acfc2cb47d0df4112fc
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-02-17 02:44:57 +0900

    수정: 1009: 로컬에서 작동하는 코드 완성. 시간초과로 제출은 실패

--- 1009.py
+++ 1009.py
@@ -1,11 +1,13 @@
 t = 0
-a, b = 0, 0
 while(t <= 0):
+    print("t값을 입력해주세요")
     t = input()
     t = int(t)
 
-while(not ((1 <= a and a <= 100) and (1<= b and b <= 1000000))):
-    a, b = input().split()
-    a, b = int(a), int(b)
-
-print((a**b)//10)
+for i in range(0, t):
+    a, b = 0, 0
+    while(not ((1 <= a and a <= 100) and (1<= b and b <= 1000000))):
+        print("a값과 b값을 공백으로 구분하여 입력해주세요")
+        a, b = input().split()
+        a, b = int(a), int(b)
+    print((a**b)-((a**b)//10)*10)
