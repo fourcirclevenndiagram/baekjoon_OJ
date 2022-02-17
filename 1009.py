241242840ebb6f93b58b6f87891fb072042b9956
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-02-17 20:09:00 +0900

    수정: 1009: 도출값 공식 수정

--- 1009.py
+++ 1009.py
@@ -1,13 +1,22 @@
 t = 0
-while(t <= 0):
+# while(t <= 0):
+#     print("t값을 입력해주세요")
+#     t = input()
+#     t = int(t)
+while(1):
     print("t값을 입력해주세요")
     t = input()
     t = int(t)
+    if(t >= 0):
+        break
 
 for i in range(0, t):
     a, b = 0, 0
-    while(not ((1 <= a and a <= 100) and (1<= b and b <= 1000000))):
+    # while(not ((1 <= a and a <= 100) and (1<= b and b <= 1000000))):
+    while(1):
         print("a값과 b값을 공백으로 구분하여 입력해주세요")
-        a, b = input().split()
-        a, b = int(a), int(b)
-    print((a**b)-((a**b)//10)*10)
+        a, b = map(int, input().split())
+        # a, b = int(a), int(b)
+        if((1 <= a and a <= 100) and (1 <= b and b <= 1000000)):
+            print((a**b)-((a**b)//10)*10)
+            break
