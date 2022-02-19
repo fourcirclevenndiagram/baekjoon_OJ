7bfd5f07c6b351d2c662c52fbf8de914f7ae83f0
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-02-20 02:22:11 +0900

    보강: 10162: 버튼 수 판정을 while문에서 if문으로 변경. 서브태스크 점수 변화 없이 그대로 100

--- 10162.py
+++ 10162.py
@@ -1,15 +1,15 @@
 t = input()
 t = int(t)
 a, b, c = 0, 0, 0
-while(t >= 300):
-    a += 1
-    t -= 300
-while(t >= 60):
-    b += 1
-    t -= 60
-while(t >= 10):
-    c += 1
-    t -= 10
+if(t >= 300):
+    a = t//300
+    t %= 300
+if(t >= 60):
+    b = t//60
+    t %= 60
+if(t >= 10):
+    c = t//10
+    t %= 10
 if(t==0):
     print(a, b, c)
 else:
