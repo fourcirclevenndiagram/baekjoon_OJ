50e2c4a9dd93b4f2d1024ce6206082d23a5ccce7
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-02-19 15:31:52 +0900

    신규: 22403

---
+++ 22403.py
@@ -0,0 +1,16 @@
+t = input()
+t = int(t)
+a = []
+saigo = ''
+henjiMachi = 0
+for i in range(0, t):
+    kotoba = input()
+    if(kotoba == 'A'):
+        henjiMachi += 1
+    elif(kotoba == 'Un'):
+        henjiMachi -= 1
+    saigo = kotoba
+if(henjiMachi == 0 and saigo == 'Un'):
+    print("YES")
+else:
+    print("NO")
