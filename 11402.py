e1ba55e03d00a1bbdc1dc06d0a9600d361737c98
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-02-19 13:29:51 +0900

    수정: 11402: 알고리즘 수정 (시간초과로 실패)

--- 11402.py
+++ 11402.py
@@ -1,4 +1,12 @@
-import scipy.special
 n, k, m = input().split()
 n, k, m = int(n), int(k), int(m)
-print(int(scipy.special.comb(n, k) % m))
+old = [0, 1, 0]
+new = []
+for i in range(0, n):
+    new.append(0)
+    for j in range(0, len(old)-1):
+        new.append(old[j] + old[j+1])
+    new.append(0)
+    old = new
+    new = []
+print(int(old[k+1] % m))
