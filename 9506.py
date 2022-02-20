864aeaa9c73756f7e410ef125df96cd81d30098a
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-02-20 17:48:46 +0900

    수정: 코드 완성(했지만 틀렸습니다)

--- 9506.py
+++ 9506.py
@@ -1,10 +1,28 @@
-n = 0
-while(n > 0):
+while(1):
     n = input()
     n = int(n)
-    i = 1
+    if(n == -1):
+        break
+    k = n
+    tot = 0
+    i = 2
     drawer = []
-    while(n > 1):
+    drawer.append(1)
+    if(n == 1):
+        print("1 is NOT perfect")
+        continue
+    while(i < n/2+1):
         if(n % i == 0):
             drawer.append(i)
             i += 1
+        else:
+            i += 1
+    for i in range(0, len(drawer)):
+        tot += drawer[i]
+    if(n != tot):
+        print("%d is NOT perfect"%k)
+    elif(n == tot):
+        print("%d = "%tot, end='')
+        for i in range(0, len(drawer)-1):
+            print("%d + "%drawer[i], end='')
+        print("%d"%drawer[i+1])
