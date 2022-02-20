84e97431baee3dcac684a7d28f5046579a0cebfb
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-02-20 20:56:01 +0900

    수정: 9506: 문자열을 저장하여 출력하는 것으로 변경(실패)

--- 9506.py
+++ 9506.py
@@ -1,6 +1,7 @@
 while(1):
     n = input()
     n = int(n)
+    ans = ''
     if(n == -1):
         break
     k = n
@@ -11,7 +12,7 @@ while(1):
     if(n == 1):
         print("1 is NOT perfect")
         continue
-    while(i < n/2+1):
+    while(i < n):
         if(n % i == 0):
             drawer.append(i)
             i += 1
@@ -20,9 +21,14 @@ while(1):
     for i in range(0, len(drawer)):
         tot += drawer[i]
     if(n != tot):
-        print("%d is NOT perfect"%k)
+        # print("%d is NOT perfect"%k)
+        ans += "%d is NOT perfect"%k
     elif(n == tot):
-        print("%d = "%tot, end='')
+        # print("%d = "%tot, end='')
+        ans += "%d = "%tot
         for i in range(0, len(drawer)-1):
-            print("%d + "%drawer[i], end='')
-        print("%d"%drawer[i+1])
+            # print("%d + "%drawer[i], end='')
+            ans += "%d + "%drawer[i]
+        # print("%d"%drawer[i+1])
+        ans += "%d"%drawer[i+1]
+    print(ans)
