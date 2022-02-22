1ede6f40a7b0abcf2947c63c3d85c1cc6baadbfa
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-02-23 00:21:15 +0900

    완성: 8718

---
+++ 8718.py
@@ -0,0 +1,16 @@
+a, b = input().split()
+a, b = int(a), int(b)
+cand = []
+cand.append(b * 4)
+cand.append(b * 2)
+cand.append(b)
+cand.append(b / 2)
+cand.append(b / 4)
+for i in range(0, 3):
+    weight = 0
+    for j in range(0, 3):
+        weight += cand[i+j]
+    if(weight <= a):
+        weight = int(weight*1000)
+        print(weight)
+        break
