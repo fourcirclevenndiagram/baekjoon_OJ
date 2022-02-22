d1b433a0b0cb42478312e94273b4725fb50ac42c
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-02-23 01:26:01 +0900

    완성: 10951

--- 10951.py
+++ 10951.py
@@ -1,9 +1,10 @@
 a, b = 0, 0
 while(1):
-    a, b = input().split()
-    a, b = int(a), int(b)
-    if((a >= 0 and a <= 10) and (b >= 0 and b <= 10)):
-        print(a+b)        
-    else:
-        continue
+    try:
+        a, b = input().split()
+        a, b = int(a), int(b)
+        if((a > 0 and a < 10) and (b > 0 and b < 10)):
+            print(a+b)        
+    except:
+        exit()
     
