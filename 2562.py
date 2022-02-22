6058afa605c40965a498367a06e04f31422aad3a
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-02-23 00:48:59 +0900

    완성: 2562

---
+++ 2562.py
@@ -0,0 +1,12 @@
+n = []
+for i in range(0, 9):
+    a = input()
+    n.append(int(a))
+max = n[0]
+max_idx = 0
+for i in range(1, 9):
+    if(n[i] > max):
+        max = n[i]
+        max_idx = i+1
+print(max)
+print(max_idx)
