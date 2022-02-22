46b2cb4efe517cc5b88e573635b1fdb8616e7af4
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-02-22 17:32:51 +0900

    수정: 7489: 런타임 에러(ValueError)

--- 7489.py
+++ 7489.py
@@ -5,12 +5,15 @@ ans = []
 for i in range(0, t):
     n = input()
     n = int(n)
+    if(n == 0):
+        ans.append(1)
+        break
     for j in range(1, n):
         n *= j
-    while(n%10 == 0):
-        n /= 10
+        if(n%10 == 0):
+            n /= 10     
     ans.append(n)
 for i in range(0, t):
     print(int(ans[i]%10))
 
-# 런타임 에러 (OverflowError)
+# 런타임 에러 (ValueError)
