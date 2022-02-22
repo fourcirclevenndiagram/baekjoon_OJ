6708c896133f7c90826d3bc92a87ec9885726625
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-02-22 17:04:26 +0900

    신규: 7489

---
+++ 7489.py
@@ -0,0 +1,14 @@
+t = input()
+t = int(t)
+
+ans = []
+for i in range(0, t):
+    n = input()
+    n = int(n)
+    for j in range(1, n):
+        n *= j
+    while(n%10 == 0):
+        n /= 10
+    ans.append(n)
+for i in range(0, t):
+    print(int(ans[i]%10))
