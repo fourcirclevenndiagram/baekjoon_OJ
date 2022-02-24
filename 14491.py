2f71e333e905d3dba667e913434d0e7af6578d6d
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-02-25 02:24:24 +0900

    수정: 14491: 알고리즘 수정중

--- 14491.py
+++ 14491.py
@@ -2,14 +2,18 @@ n = input()
 n = int(n)
 k = n
 ans = []
-while(n > 9):
-    C = 9
+while(1):
+    C = 1
     i = 0
-    while(n > C):
+    while(1):
         C *= 9
-        i += 1
-    ans.append(str(n//(9**i)))
-    n = n % (9**i)
+        if(n > C):
+            i += 1
+            continue
+        C /= 9
+        
+
+
 ans.append(str(n))
 ans = ''.join(ans)
 ans = int(ans)
