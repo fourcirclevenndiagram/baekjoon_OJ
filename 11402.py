38265419312fc949b0799e2a98aefbef5cd86552
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-02-24 23:34:12 +0900

    수정: 필요한 만큼만 구하고 그 뒤는 자르는 코드 추가(결과는 또 시간초과)

--- 11402.py
+++ 11402.py
@@ -1,8 +1,7 @@
-n, k, m = input().split()
-n, k, m = int(n), int(k), int(m)
+n, k, m = map(int, input().split())
+
 # old = [0, 1, 0]
-old = [0, 1]
-new = []
+# new = []
 # for i in range(0, n):
 #     new.append(0)
 #     for j in range(0, len(old)-1):
@@ -10,8 +9,19 @@ new = []
 #     new.append(0)
 #     old = new
 #     new = []
-for i in range(0, int(n/2)+1):
-    if(n % 2 == 1):
-        break
+#     print(old)
+# 
+# print(int(old[k+1] % m))
 
+old = [0, 1, 0]
+new = []
+for i in range(0, n):
+    new.append(0)
+    for j in range(0, len(old)-1):
+        new.append(old[j] + old[j+1])
+        if(j == k):
+            break
+    new.append(0)
+    old = new
+    new = []
 print(int(old[k+1] % m))
