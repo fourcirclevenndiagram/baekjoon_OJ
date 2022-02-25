583f716b9c1e233bed900c30c556fb3184b79490
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-02-25 23:30:51 +0900

    수정: 2839: 틀렸습니다

---
+++ 2839.py
@@ -0,0 +1,13 @@
+n = int(input())
+max_cnt = n // 3 + 1
+flag = 0
+ans = 0
+for i in range(max_cnt):
+    for j in range(max_cnt-i):
+        if(n == i*3 + j*5):
+            ans = i+j
+            flag = 1
+            break
+    if(flag):
+        print(ans)
+        break
