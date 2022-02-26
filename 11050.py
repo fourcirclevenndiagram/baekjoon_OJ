13585012decd9fbe307a9fa25ed1b698fa6834cf
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-02-27 07:08:41 +0900

    완성: 11050

---
+++ 11050.py
@@ -0,0 +1,27 @@
+n, k = map(int, input().split())
+
+# old = [0, 1, 0]
+# new = []
+# for i in range(0, n):
+#     new.append(0)
+#     for j in range(0, len(old)-1):
+#         new.append(old[j] + old[j+1])
+#     new.append(0)
+#     old = new
+#     new = []
+#     print(old)
+# 
+# print(int(old[k+1] % m))
+
+old = [0, 1, 0]
+new = []
+for i in range(0, n):
+    new.append(0)
+    for j in range(0, len(old)-1):
+        new.append(old[j] + old[j+1])
+        if(j == k):
+            break
+    new.append(0)
+    old = new
+    new = []
+print(int(old[k+1]))
