c522c3b7cba1e5ad05188b8580de50246c650602
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-03-04 00:49:59 +0900

    완성: 11047

--- 11047.py
+++ 11047.py
@@ -1,14 +1,16 @@
-n, k = map(int, input().split())
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
 my_coin = []
 pay = 0
 for i in range(0, n):
-    temp = int(input())
+    temp = int(sys.stdin.readline())
     my_coin.append(temp)
 i = 0
 while(k > 0):
-    if(k >= my_coin[len(my_coin)-i-1]):
-        k -= my_coin[len(my_coin)-i-1]
-        pay += 1
+    if(k >= my_coin[len(my_coin)-i-1]):        
+        pay += k // my_coin[len(my_coin)-i-1]
+        k %= my_coin[len(my_coin)-i-1]
     else:
         i += 1
 print(pay)
