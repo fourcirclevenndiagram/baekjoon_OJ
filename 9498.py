52dd31b4c08906c847cc65be224ca3cb08034bbb
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-03-07 10:43:45 +0900

    완성: 9498

---
+++ 9498.py
@@ -0,0 +1,11 @@
+n = int(input())
+if(100 >= n >= 90):
+    print("A")
+elif(89 >= n >= 80):
+    print("B")
+elif(79 >= n >= 70):
+    print("B")
+elif(69 >= n >= 60):
+    print("B")
+else:
+    print("F")
