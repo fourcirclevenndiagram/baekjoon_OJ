b6dcb3f4c38d9609f063d6258133c4a8b700a520
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-03-19 23:33:36 +0900

    완성: 1157

--- 1157.py
+++ 1157.py
@@ -1,9 +1,11 @@
-my_word = input()
-for_cnt_word = list(set(my_word.upper()))
+my_word = input().upper()
+for_cnt_word = list(set(my_word))
 for_cnt_cnt = []
+
 
 for i in range(len(for_cnt_word)):
     for_cnt_cnt.append(my_word.count(for_cnt_word[i]))
+    print(for_cnt_cnt)
 if(for_cnt_cnt.count(max(for_cnt_cnt)) > 1):
     print('?')
 else:
