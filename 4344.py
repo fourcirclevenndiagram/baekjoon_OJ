aab3519ee05cc01b1fa6106343c59fc173c273bd
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-03-24 14:13:39 +0900

    완성: 4344

--- 4344.py
+++ 4344.py
@@ -3,4 +3,10 @@ for i in range(c):
     temp = list(map(int, input().split()))
     scores = temp[1:]
     average = sum(scores) / len(scores)
-    print("%d%%"%(round(average, 3)))
+    winner = 0
+    for j in scores:
+        if(j > average):
+            winner += 1
+    ans = winner / len(scores) * 100
+    # print("%d%%"%(round(ans, 3)))
+    print(f'{ans:.3f}%')
