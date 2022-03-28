cf453a93d7671dd17595efa232383cac07ed3875
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-03-29 02:20:28 +0900

    신규: 2581: 틀림

---
+++ 2581.py
@@ -0,0 +1,21 @@
+m = int(input())
+n = int(input())
+prime_num = []
+ans = 0
+for i in range(m, n+1):
+    flag = 0
+    if(i > 1):
+        for j in range(2, i):
+            if(not (i%j)):
+                flag = 1
+                break
+        if(not flag):
+            prime_num.append(i)
+
+if(flag):
+    print(-1)
+else:
+    for i in prime_num:
+        ans += i
+    print(ans)
+    print(prime_num[0])
