fa5942c241be18c8def380411b55a81dc61f4e55
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-03-29 02:05:03 +0900

    수정: 2805: 탐색 알고리즘 수정, 틀림

--- 2805.py
+++ 2805.py
@@ -2,16 +2,24 @@ import sys
 input = sys.stdin.readline
 n, m = map(int, input().split())
 trees = list(map(int, input().split()))
-start = max(trees)
-axe = start
-flag = 0
-while(1):
-    gain_tree = 0
+start, end = max(trees), 0
+# while(1):
+#     gain_tree = 0
+#     for i in trees:
+#         if(i > axe):
+#             gain_tree += (i - axe)
+#     if(gain_tree > m):
+#         flag = 1
+#         break
+#     axe -= 1
+while(start > end):
+    mid = (start+end) // 2
+    cut_tree = 0
     for i in trees:
-        if(i > axe):
-            gain_tree += (i - axe)
-    if(gain_tree > m):
-        flag = 1
-        break
-    axe -= 1
-print(axe+1)
+        if(i > mid):
+            cut_tree += (i - mid)
+    if(cut_tree > m):
+        end = mid+1
+    else:
+        start = mid-1
+print(start)
