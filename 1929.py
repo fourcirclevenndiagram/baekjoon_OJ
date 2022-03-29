f18687ca721b1c9b876c7eba3e066c76f70af831
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-03-29 19:59:56 +0900

    수정: 1929: i의 제곱근까지만 처리하도록 수정: 틀림

--- 1929.py
+++ 1929.py
@@ -1,12 +1,14 @@
-m, n = map(int, input().split())
+import sys
+m, n = map(int, sys.stdin.readline().split())
 prime_lst = []
 for i in range(m, n+1):
     flag = 0
-    for j in range(2, i):
+    for j in range(2, int(i**0.5)+1):
         if(i%j == 0):
             flag = 1
             break
     if(not flag):
         prime_lst.append(i)
-print(prime_lst)
+for i in prime_lst:
+    print(i)
     
