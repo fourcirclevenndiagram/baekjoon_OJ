09de44808b0663351617ce59bad720afaf2c1159
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-03-30 07:57:12 +0900

    완성: 1920

--- 1920.py
+++ 1920.py
@@ -1,8 +1,23 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-a = list(map(int, input().split()))
+a = sorted(list(map(int, input().split())))
 m = int(input())
 b = list(map(int, input().split()))
+
+def bin_search(start, end, unit, the_list):
+    if start > end:
+        return 0
+    mid = int((start+end)/2)
+    if(the_list[mid] == unit):
+        return 1
+    elif(the_list[mid] > unit):
+        return bin_search(start, mid-1, unit, the_list)
+    else:
+        return bin_search(mid+1, end, unit, the_list)
+
 for i in b:
-    print(a.count(i))
+    start = 0
+    end = len(a)-1
+    ans = bin_search(start, end, i, a)
+    print(ans)
