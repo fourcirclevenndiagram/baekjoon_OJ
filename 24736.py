3255fb3e73ea3a37d0b4bb0e4d187e2fa090b5d6
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-05-18 15:23:17 +0900

    완성: 24736

---
+++ 24736.py
@@ -0,0 +1,7 @@
+drawer = []
+t, f, s, p, c = list(map(int, input().split()))
+t1 = t*6 + f*3 + s*2 + p + c*2
+t, f, s, p, c = list(map(int, input().split()))
+t2 = t*6 + f*3 + s*2 + p + c*2
+
+print(t1, t2)
