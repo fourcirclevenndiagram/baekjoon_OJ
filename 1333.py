b2b8a3f8720c971af21a2a4fd22ffa3b62e1a815
fourcirclevenndiagram <unchainedsoul@nate.com>
2022-08-03 19:38:29 +0900

    완성: 1333

---
+++ 1333.py
@@ -0,0 +1,12 @@
+n, l, d = map(int, input().split())
+check = [False] * (n * l + 5 * (n - 1))
+for i in range(n):
+    s = (l + 5) * i
+    for j in range(s, s + l):
+        check[j] = True
+answer = 0
+while answer < len(check):
+    if check[answer] == 0:
+        break
+    answer += d
+print(answer)
